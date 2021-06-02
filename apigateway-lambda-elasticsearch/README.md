492f2e34b1dba6fc08109e024db713553302dfe0
hedykim <waltzbox@gmail.com>
2021-06-02 10:26:34 +0900

    add new sample code

---
+++ apigateway-lambda-elasticsearch/README.md
@@ -0,0 +1,31 @@
+# Lambda function: Elasticsearch service data query via API Gateway service
+
+AWS API Gateway 서비스를 통해서 Elasticsearch service query 를 전달 하는 샘플 코드 입니다.
+
+# Getting start
+## Requirements
+* git 이 필요 합니다.
+* Python 3.8 기준 으로 작성 되었습니다.
+* pip3(package installer for Python3) 가 필요 합니다.
+* Python reference module elasticsearch 와 aws_requests_auth 가 필요합니다.
+
+##  Repository download
+* code download
+
+        git clone --depth 1 --filter=blob:none --no-checkout https://github.com/waltzbucks/aws-lambda-functions
+        cd aws-lambda-functions/
+        git checkout master -- apigateway-lambda-elasticsearch
+
+
+
+## Code packaging
+1. build.sh 를 사용하여 python virtual environment 를 setup 합니다.
+
+        sh ./build.sh -s
+
+2. _venv directory 가 생성 되었으면, 아래 명령으로 package zip 파일을 만듭니다.
+
+        sh ./build.sh -b
+
+3. 생성된 zip 파일을 Lambdb function code 에 업로드하여 사용 할 수 있습니다.
+<br>e.g lambda-image-convert-1614911870.zip
---
+++ apigateway-lambda-elasticsearch/lambda_function.py
@@ -0,0 +1,112 @@
+import json
+import logging
+import boto3
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch import helpers
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+
+
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+
+logging.basicConfig(format = '%(created)f:%(levelname)s:%(funcName)s:%(message)s', level=logging.INFO)
+
+
+def load_config(context):
+    
+    logging.info(context)
+    
+    # Check version
+    function_name = context.function_name
+    alias = context.invoked_function_arn.split(':').pop()
+
+    if function_name == alias:
+        alias = '$LATEST'
+        print("No Version Set - Default to $LATEST")
+
+
+    # Set config
+    with open('es-access-config.json') as json_data:
+        config = json.load(json_data)
+
+    print("Succesfully loaded config")
+    return config
+
+def sts_auth(config):
+    logging.info(config)
+    
+    # Genarate auth request to connect AWS ES
+    sts = boto3.client('sts')
+
+    creds = sts.assume_role(
+        RoleArn=config['sts_role_arn'], RoleSessionName=config['sts_session_name'])
+
+    auth = AWSRequestsAuth(aws_access_key=creds['Credentials']['AccessKeyId'],
+                           aws_secret_access_key=creds['Credentials']['SecretAccessKey'],
+                           aws_host=config['es_host'],
+                           aws_region=config['es_region'],
+                           aws_service='es',
+                           aws_token=creds['Credentials']['SessionToken'])
+    return auth
+
+def write_bulk(record_set, es_client, config):
+    logging.info("Writing data to ES")
+    
+    # Write the data set to ES, chunk size has been increased to improve performance
+    
+    resp = helpers.bulk(es_client,
+                        record_set,
+                        chunk_size=config['es_bulk_chunk_size'],
+                        timeout=config['es_bulk_timeout'])
+    return resp
+
+def datascan(es_client,event):
+    logging.info("search the data to ES")
+    logging.info("ES Client=" + es_client)
+    logging.info("Event=" + event)
+    
+    searchquery = queries(event)
+    resp = helpers.scan(es_client,
+                        # query={"query": {"match": {"_id": "TvyPpnkBkZlE89-bOEMP"}}},
+                        query={"query": {"match": searchquery}},
+                        index="kibana_sample_data*")
+    
+    results_list = []
+    for hit in resp:
+        print(hit)
+        results_list.append(hit["_source"])
+    
+    return results_list
+
+def queries(event):
+    
+    # TODO: search query processing
+    searchquery = event
+    
+    logging.info("search query=" + event)
+    
+    return searchquery
+    
+    
+def lambda_handler(event, context):
+    logging.info(event)
+    logging.info(context)
+    
+    # Invoke Lambda
+    # load config 
+    config = load_config(context)
+        
+    # create ES connection with sts auth file
+    es_client = Elasticsearch(host=config['es_host'],
+                              scheme="https",
+                              port=443,
+                              http_auth=sts_auth(config),
+                              timeout=config['es_connection_timeout'],
+                              connection_class=RequestsHttpConnection)
+    
+    resp = json.dumps(datascan(es_client,event))
+    logging.info("Return=" + resp)
+    
+    return resp
